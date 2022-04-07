24d5d30c5bc4997ce2643636aa5ab24f9fddb6bb
Daris Strickland <daris.strickland@gmail.com>
2022-04-06 21:38:37 -0400

    clean up

--- init.lua
+++ init.lua
@@ -1,13 +1,8 @@
 require("user.options")
-vim.cmd("source ~/.config/nvim/vimscript_extra.vim")
-
 require("user.keymaps")
-vim.cmd("source ~/.config/nvim/remaps.vim")
-
 require("user.plugins")
 require("user.colorscheme")
 require("user.cmp")
-
 require("alpha")
 require("user.lsp")
 require("user.lint")
