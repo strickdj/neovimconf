84d27b7e332561692faa136a683416a96d13745c
Daris Strickland <daris.strickland@gmail.com>
2022-04-07 22:02:07 -0400

    fix format on save, virtual text

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -31,7 +31,7 @@ vim.cmd([[
 
   augroup _lsp
     autocmd!
-    autocmd BufWritePre * lua vim.lsp.buf.formatting()
+    autocmd BufWritePre * lua vim.lsp.buf.formatting_seq_sync()
   augroup end
 ]])
 
